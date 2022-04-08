ec0147370f23bc20bfdf11555e3303ac82652ca5
JitinKGoyal <93308568+JitinKGoyal@users.noreply.github.com>
2022-04-08 17:29:26 +0530

    change in name of Stepdefinition

--- src/test/java/com/synectiks/procurement/cucumber/stepDefinition/Stepdefinition.java
+++ src/test/java/com/synectiks/procurement/cucumber/stepDefinition/Stepdefinition.java
@@ -16,7 +16,7 @@ public class Stepdefinition {
 	WebDriver driver1;
 
 	@Given("^user is going on procurment login page$")
-	public void user_is_going_on_gmail() {
+	public void user_is_going_on_procurment_login_page() {
 		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\cucumberDriver\\chromedriver1.exe");
 		driver1 = new ChromeDriver();
 		driver1.manage().window().maximize();
@@ -24,17 +24,17 @@ public class Stepdefinition {
 	}
 
 	@Then("^click on submit$")
-	public void click_on_create_account() throws Throwable {
+	public void click_on_submit() throws Throwable {
 		driver1.findElement(By.className("MuiButton-fullWidth")).click();
 	}
 
 	@Then("^click on new requisition$")
-	public void click_on_for_myself() throws Throwable {
+	public void click_on_new_requisition() throws Throwable {
 		driver1.findElement(By.xpath("//a[@href='/postlogin/newrequisition']")).click();
 	}
 
 	@And("^user fill all input fields$")
-	public void user_input_firstname_and_lastname_and_username() {
+	public void user_fill_all_input_fields() {
 		driver1.findElement(By.name("roleName")).sendKeys("Jitin");
 
 		Select department = new Select(driver1.findElement(By.name("departmentId")));
@@ -48,14 +48,14 @@ public class Stepdefinition {
 	}
 
 	@Then("^click on add new item$")
-	public void user_input_password_and_confirm_password() {
+	public void click_on_add_new_item() {
 
 		driver1.findElement(By.className("add_new_item")).click();
 
 	}
 
 	@And("^fill all item input values and click on save$")
-	public void click_on_next() {
+	public void fill_all_item_input_values_and_click_on_save() {
 		driver1.findElement(By.name("itemDescription")).sendKeys("demo description");
 		driver1.findElement(By.name("ratePerItem")).sendKeys("124");
 		driver1.findElement(By.name("itemQuantity")).sendKeys("2");
@@ -64,7 +64,7 @@ public class Stepdefinition {
 	}
 
 	@Then("^click on send$")
-	public void wait_for_three_seconds() {
+	public void click_on_send() {
 		driver1.findElement(By.className("send_requi")).click();
 		driver1.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
